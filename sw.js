9d81213a5836e81768bb876cee47d8a3a1ecd342
mgnong-ctrl <mgnong@gmail.com>
2025-12-30 14:16:37 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,18 +1,15 @@
-const CACHE_NAME = 'word-quiz-v2';
+const CACHE_NAME = 'word-quiz-v5';
 const ASSETS = [
   'index.html',
+  'history.html',
   'test/syo.html',
   'data/Elementary English Vocabulary-sheet2api.com.json'
 ];
 
 self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-  );
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
 });
 
 self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((res) => res || fetch(e.request))
-  );
+  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
 });
