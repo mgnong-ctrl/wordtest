561e756b41d60ed93c6168fa47254692947e28fa
mgnong-ctrl <mgnong@gmail.com>
2025-12-30 17:24:47 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -2,8 +2,28 @@ const CACHE_NAME = 'word-quiz-v5';
 const ASSETS = [
   'index.html',
   'history.html',
-  'test/syo.html',
-  'data/Elementary English Vocabulary-sheet2api.com.json'
+  'test/test1-1.html',
+  'test/test2-1.html',
+  'test/test2-2.html',
+  'test/test2-3.html',
+  'test/test2-4.html',
+  'test/test2-5.html',
+  'test/test2-6.html',
+  'test/test2-7.html',
+  'test/test2-8.html',
+  'test/test2-9.html',
+  'test/test3-1.html',
+  'data/data1-1.json',
+  'data/data2-1.json',
+  'data/data2-2.json',
+  'data/data2-3.json',
+  'data/data2-4.json',
+  'data/data2-5.json',
+  'data/data2-6.json',
+  'data/data2-7.json',
+  'data/data2-8.json',
+  'data/data2-9.json',
+  'data/data3-1.json'
 ];
 
 self.addEventListener('install', (e) => {
